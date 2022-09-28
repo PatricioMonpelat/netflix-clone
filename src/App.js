76c6e0a3bbe7761694455a3f7c45ad0d879d4451
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-09-28 12:03:27 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
 import './App.css';
+import Row from './Row'
+import requests from '../src/request';
+import Banner from '../src/Banner';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello</h1>
+    <div className="app">
+      {/* {NAV} */}
+     <Banner/>
+     <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+     <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+     <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+     <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+     <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
     </div>
   );
 }
