bfce7357c012ba71a25fd61191b13603eca0448a
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-09-28 14:33:59 -0300

    updates

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
 
 function Nav() {
+  
+  const [show, handleShow] = useState(false);
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     if (window.scrollY > 100) {
+  //       handleShow(true);
+  //     } else handleShow(false);
+  //   });
+  //   return () => {
+  //     window.removeEventListener("scroll");
+  //   };
+  // }, []);
+
   return (
-    <div>Nav</div>
-  )
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
