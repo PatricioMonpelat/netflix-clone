1b9b5d7b60ff405f93f49329de0832830755e421
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-10-03 16:22:54 -0300

    sign up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import './App.css';
-import Row from './Row'
-import requests from '../src/request';
-import Banner from '../src/Banner';
-import Nav from '../src/Nav';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
-    <div className="app">
-     <Nav/>
-     <Banner/>
-     <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-     <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-     <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-     <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-     <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
-
+    <div className='app'>
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path='/' element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
