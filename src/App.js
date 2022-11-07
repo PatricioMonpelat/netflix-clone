80a8a588caddc65ffe8353192089ab31a32b5db3
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-11-07 14:07:59 -0300

    react-redux installation

--- src/App.js
+++ src/App.js
@@ -3,31 +3,40 @@ import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
-import { auth } from './firebase';
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { logout, login } from "./features/userSlice";
 
 function App() {
   const user = null;
-  
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         // logged in
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         // logged out
+        dispatch(logout);
       }
     });
     return unsubscribe;
   }, []);
 
   return (
-    <div className="app">
+    <div className='app'>
       <Router>
         {!user ? (
           <LoginScreen />
         ) : (
           <Routes>
-            <Route exact path="/" element={<HomeScreen />} />
+            <Route exact path='/' element={<HomeScreen />} />
           </Routes>
         )}
       </Router>
